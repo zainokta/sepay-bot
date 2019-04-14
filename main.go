9df5b8b097f4dd189aed978768e5f7eb855d7d80
zainokta <zainokta@gmail.com>
2019-04-14 13:13:39 +0700

    fix

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+// Copyright 2016 LINE Corporation
+//
+// LINE Corporation licenses this file to you under the Apache License,
+// version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at:
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
 package main
 
 import (
@@ -5,16 +19,11 @@ import (
 	"net/http"
 	"os"
 
-	"google.golang.org/appengine"
-	aelog "google.golang.org/appengine/log"
-	"google.golang.org/appengine/urlfetch"
-
 	"github.com/line/line-bot-sdk-go/linebot"
-	"github.com/line/line-bot-sdk-go/linebot/httphandler"
 )
 
-func init() {
-	handler, err := httphandler.New(
+func main() {
+	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_TOKEN"),
 	)
@@ -23,23 +32,30 @@ func init() {
 	}
 
 	// Setup HTTP Server for receiving requests from LINE platform
-	handler.HandleEvents(func(events []*linebot.Event, r *http.Request) {
-		ctx := appengine.NewContext(r)
-		bot, err := handler.NewClient(linebot.WithHTTPClient(urlfetch.Client(ctx)))
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
 		if err != nil {
-			aelog.Errorf(ctx, "%v", err)
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
 			return
 		}
 		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).WithContext(ctx).Do(); err != nil {
-						aelog.Errorf(ctx, "%v", err)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
 					}
 				}
 			}
 		}
 	})
-	http.Handle("/callback", handler)
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
 }
